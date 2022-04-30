58d0ef0641c3ada450abc38ba8836f4469f8c875
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2022-04-30 17:50:02 +0900

    Adapt the responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 'use strict';
-// const menuBtn = document.getElementById('menu-btn');
-// menuBtn.addEventListener('click', function() {
-//   menuBtn.classList.toggle('is-active');
-// })
+const menuBtn = document.getElementById('menu-btn');
+const overlay = document.querySelector('.overlay-sp');
+const menuText01 = document.getElementById('menu-text01');
+const menuText02 = document.getElementById('menu-text02');
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('is-active');
+  overlay.classList.toggle('is-open');
+  menuText01.classList.toggle('is-clicked');
+  menuText02.classList.toggle('is-clicked');
+})
